b3b2d6f5d52c29edc4fdaa3505e1230ffdae092f
Thomas Sillmann <thomas@thomassillmann.de>
2015-09-29 11:25:44 +0200

    Initial Commit

---
+++ TSLoggingKit/TSLoggingKit.h
@@ -0,0 +1,19 @@
+//
+//  TSLoggingKit.h
+//  TSLoggingKit
+//
+//  Created by Thomas Sillmann on 29.09.15.
+//  Copyright © 2015 Thomas Sillmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TSLoggingKit.
+FOUNDATION_EXPORT double TSLoggingKitVersionNumber;
+
+//! Project version string for TSLoggingKit.
+FOUNDATION_EXPORT const unsigned char TSLoggingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TSLoggingKit/PublicHeader.h>
+
+
